f80eb8ed380f3f3ffeb06647ed6c7fb540624681
Brian Wo <45139213+brainwo@users.noreply.github.com>
2022-10-24 00:18:38 +0800

    feat: date api

--- index.js
+++ index.js
@@ -14,14 +14,27 @@ app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 20
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get("/", function (_, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// get date
+// returns `unix time epoch and utc formatted date
+app.get("/api/:date?", function (req, res) {
+  const param = req.params.date;
+  var date = new Date(param === undefined ? Date.now() : param);
+
+  if (isNaN(date.getDate())) {
+    date.setTime(param);
+  }
+
+  if (!isNaN(date.getDate())) {
+    res.json({unix: date.getTime(), utc: date.toUTCString()});
+    return;
+  }
+
+  res.json({error: "Invalid Date"});
 });
 
 
